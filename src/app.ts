3451114f5a1948828b25a32be31e09a9102cc3d0
luckyboy07 <cairudin.16@gmail.com>
2026-04-14 21:45:13 +0800

    added allowedOrigins

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://car-rental-bohol-frontend.vercel.app'
+]
+// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
